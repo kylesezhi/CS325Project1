88a480e885ce7078c6f5655e14e6e1cf3d4cdc19
Tom <t.adamcewicz@gmail.com>
2016-07-06 16:49:19 -0400

    revised dAndC
    Got rid of excess code in divideAndConquer.py

--- divideAndConquer.py
+++ divideAndConquer.py
@@ -1,18 +1,3 @@
-def enumeration(array):
-        maxSum = 0
-        for i in range(len(array)):
-            for j in range(i,len(array)):
-                currentSum = sumArray(array,i,j)
-                if currentSum > maxSum: maxSum = currentSum
-        return maxSum
- 
-def sumArray(array,i,j):
-    sum = 0
-    for x in range(i,j+1):
-        sum = sum + array[x]
-    return sum
- 
-print enumeration([31, -41, 59, 26, -53, 58, 97, -93, -23, 84]) #187
 
 def maxCrossingSum(array, l, m, h):
     thesum = 0
