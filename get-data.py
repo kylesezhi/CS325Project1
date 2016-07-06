096c3b5b5a33a7a74340409aaeacf55d28b37540
Tom <t.adamcewicz@gmail.com>
2016-07-06 17:19:18 -0400

    updated get-data.py
    - Added divideAndConquer test get-data.py
    
    - Added get_DNC_data function in get-data.py for divideAndConquer
    algorithm.
    
    - Ran divideAndConquer test and generated divideAndConquer.csv

--- get-data.py
+++ get-data.py
@@ -5,7 +5,7 @@ import csv
 from random import randint
 import enumeration
 import better_enumeration
-# import divide_and_conquer
+import divideAndConquer
 import linear_time # Steven
 
 def makeRandomList(n): # n is the length of the returned list
@@ -32,7 +32,26 @@ def getData(function, r, filename):
 		for line in data:
 			writer.writerow(line)
 			
+
+def get_DNC_Data(function, r, filename):
+	print '\n' + filename
+	# GATHERING DATA
+	data = []
+	for x in r:
+		A = makeRandomList(x)
+		start = time.clock()
+		function(A, 0, len(A) -1)
+		end = time.clock()
+		speed = end - start
+		data.append([x,speed])
+		print(str(x) + ": " + str(end - start))
+	# WRITING TO CSV
+	with open(filename, 'wb') as f:
+		writer = csv.writer(f, delimiter=',')
+		for line in data:
+			writer.writerow(line)
 #-------------------------------------------------------------------------------
 getData(enumeration.enumeration, range(100,1001,100), 'enumeration.csv')
 getData(better_enumeration.better_enumeration, range(1000,10001,1000), 'better_enumeration.csv')
 getData(linear_time.linear_time, range(100000,1000001,100000), 'linear_time.csv')
+get_DNC_Data(divideAndConquer.maxSubArraySum, range(100000,1000001,100000), 'divideAndConquer.csv')
